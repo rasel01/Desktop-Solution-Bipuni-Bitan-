6b97ba145d4976b0dfc50e1bca24f9e662c7391b
rasel01 <raseluiu.ac.bd@gmail.com>
2016-08-24 01:18:53 +0600

    measurementManager added

--- BipuniBitan_Manager/Setup/MessurmentManager.cs
+++ BipuniBitan_Manager/Setup/MessurmentManager.cs
@@ -6,8 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BipuniBitan_DB;
-using BipuniBitan_Manager;
-using BipuniBitan_Manager;
+using BipuniBitan_Manager.Utility;
 
 namespace BipuniBitan_Manager.Setup
 {
